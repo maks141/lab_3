510cc15dea14733c5a1b68b109650b1f1cb601ef
maks141 <books141@mail.ru>
2022-10-09 17:36:35 +0500

    Интерфейс

--- Фт-210007Спиридонов.py
+++ Фт-210007Спиридонов.py
@@ -1,9 +1,30 @@
-# from curses.ascii import isupper
+abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+while True:
+    input_string = input('Шифр цезаря. Введите на английском строку, которую хотите зашифровать/расшифровать: ')
+    c = 0
+    for i in input_string.lower():
+        if i in abc:
+            c += 1
+        else:
+            print('Введите строку на англиском языке!')
+            break
+    if len(input_string) == c:
+        break
 
-input_string = input('Шифр цезаря. Введите на английском строку, которую хотите зашифровать/расшифровать: ')
-quest = input('Хотите зашифровать или дешифровать? (введите 1 или 2): ')
-step = int(input('Введите шаг сдвига: '))
+while True:
+    quest = input('Хотите зашифровать или дешифровать? (введите 1 или 2): ')
+    if quest == '1' or quest == '2':
+        break
+    else:
+        print('Введите 1 или 2!')
+
+while True:
+    step = int(input('Введите шаг сдвига: '))
+    if step <= 0:
+        print('Введите положительный шаг сдвига!')
+    else:
+        break
 
 string = input_string.lower()
 
@@ -36,6 +57,7 @@ def MakeNewString():
 string_out = []
 for i in MakeNewString():
     string_out.append(i)
+
 
 for i in input_string:
     if i.isupper():
